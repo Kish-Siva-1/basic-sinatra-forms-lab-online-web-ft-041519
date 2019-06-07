b2ea2e4477aae8d469229f32e628caccba8c1d11
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-06-07 13:02:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,10 +2,12 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
   
-  post '/newteam' do
-    params
-   
+  get '/newteam' do
     erb :newteam
+  end
+  
+  post '/team' do
+    erb :team
   end
 
 end
