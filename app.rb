9c0bff9b80fdbce98539a5e313ab3028c0d9e5d5
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-06-07 14:24:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,11 @@ class App < Sinatra::Base
   end
   
   post '/team' do
-    @
+    @name = params[:name]
+    @name = params[:name]
+    @name = params[:name]
+    @name = params[:name]
+    @name = params[:name]
     erb :team
   end
 
